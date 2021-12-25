9772c74dffa1932c08beb7b20229b2caed9a1500
Zephrineart <60871731+Zephrineart@users.noreply.github.com>
2021-12-25 00:45:24 -0800

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,8 @@ from adafruit_circuitplayground.express import cpx
 
 while True:
     if cpx.touch_A2:
-        cpx.play_file("cat.wav")
+        cpx.play_file("purr.wav")
+    elif cpx.touch_A4:
+        cpx.play_file("meow.wav")
     else:
         cpx.stop_tone()
